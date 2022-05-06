85540bf3b51c7f267cbf6ed6201267ff09d41b53
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-05-06 00:52:07 -0300

    Repositorio criado

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Course
+{
+    class Classes
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4"));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4"));
+        }
+    }
+}
