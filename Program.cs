68f9e49b502227a64fb829abb8fa078bb548ba22
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-05-06 01:16:54 -0300

    Repositorio criado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divison by 0 not allowed!");
+            }
+            catch (FormatException p)
+            {
+                Console.WriteLine("Error! " + p.Message);
+            }
+            finally
+            {
+                
+            }
+        }
+    }
+}
