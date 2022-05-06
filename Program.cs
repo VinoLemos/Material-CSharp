6d353c3136314774bcc072146e240bd556990974
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-05-06 01:17:16 -0300

    Repositorio criado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace UsingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/tmp/File.txt";
+
+            try
+            {
+                // Ao final do bloco, todos os recursos serão fechados automaticamente
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
