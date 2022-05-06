fb7dad1ba4a2d347445d3115af0bd73358762a17
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-05-06 13:45:28 -0300

    Classe Action

---
+++ Action/Entities/Product.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Globalization;
+
+namespace Action.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price {get; set;}
+
+                public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        
+        public string GetPrice()
+        {
+            return "R$ " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return GetName() + ", " + GetPrice();
+        }
+
+        /* public int CompareTo(Product? other)
+        {
+            return GetName().ToUpper().CompareTo(other.GetName().ToUpper());
+        } */
+    }
+}
---
+++ Action/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Action.Entities;
+
+namespace Action
+{
+    class Program
+    {
+        /* 
+                Fazer um programa que, a partir de uma lista de produtos, aumente o preço dos produtos em 10%.
+         */
+
+        static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.00));
+
+            /*
+            Utilizando Delegate
+             Action<Product> act = UpdatePrice;
+            list.ForEach(act); */
+
+            //list.ForEach(UpdatePrice); Método só aceita uma função void como parâmetro
+
+            // Expressão Lambda
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
