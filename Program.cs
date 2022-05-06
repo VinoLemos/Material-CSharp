082c0d535a3bb40d37b2334eede6587faed23039
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-05-06 00:50:52 -0300

    Repositorio criado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ClassePath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/tmp/File1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+    }
+}
