72c575afddb0ce52aa1bd36d521503fb11643579
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-05-06 00:57:36 -0300

    Repositorio criado

---
+++ Ex01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Ex01
+    {
+        /*
+            Checklist:
+                        -   Ler um número inteiro
+                        -   Ler um caractere
+                        -   Ler um número double
+                        -   Ler um nome (única palavra), sexo (caractere F ou M), idade (inteiro)
+                            e altura (double) na mesma linha, armazenando-os em quatro variáveis
+                            com os devidos tipos
+        */
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());// Recebe uma string e converte para int
+            char ch = char.Parse(Console.ReadLine());// Recebe uma string e converte para char
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);// Recebe uma string e converte para double
+            
+            string dados = Console.ReadLine();
+            string[] vet = dados.Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3]);
+
+
+            Console.WriteLine("Você digitou: ");
+
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2);
+
+            Console.WriteLine("Dados: ");
+            Console.WriteLine("{0}, sexo {1}, {2} anos de idade, {3:F2} m de altura", nome, sexo, idade, altura);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Course
+{
+            /*
+            Checklist
+                    - Ler um texto até a quebra de linha e armazenar em uma variável
+                    - Ler três palavras, uma em cada linha, armazenando cada uma em uma variável
+                    - Ler três palavara na mesma linha, separadas por espaço, armazenando cada 
+                    uma em uma variável
+            */
+    class EntradaDeDados
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();// Lê até a quebra de linha
+
+            string x = Console.ReadLine(); // Lê uma palavra por linha
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            string s = Console.ReadLine(); // String recebe uma frase
+
+            string[] vet = s.Split(' '); // Divide a string onde houverem espaços em branco
+            string a = vet[0]; // Recebe a primeira posição do vetor
+            string b = vet[1]; // Recebe a segunda posição do vetor
+            string c = vet[2]; // Recebe a terceira posição do vetor
+            Console.WriteLine("Você digitou: " + frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
+
