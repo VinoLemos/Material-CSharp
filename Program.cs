d011a8f4629efe1a783952495ae92fb08fef2574
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2022-05-06 00:46:49 -0300

    Repositorio criado

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green";
+            object obj1 = "Alex Green";
+            object obj2 = 4.5;
+            double saldo = 10.35784;
+            int idade = 32;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(saldo.ToString("F2"));//Imprime limitando à duas casas decimais
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));//Altera o separador decimal
+            
+            // Imprimindo várias variáveis
+            // Placeholders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+            // Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+            // Concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais");
+
+
+        }
+    }
+}
